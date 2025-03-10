4d126fc33f634f50d9d9671180da3eecafd06498
100percentibrahim <ibrahimcpersonal@gmail.com>
2025-03-11 02:02:24 +0700

    refactor: define crawler activities & access

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation, Navigate } from "react-router-dom";
 import useAuth from "./libs/guards/auth";
 import WorkerUpdater from "./components/feedback/worker-updater";
 import FallbackPage from "./pages/fallback";
+import RePage from "./pages/re";
 import HomePage from "./pages/home";
 import ErrorPage from "./pages/error";
 import InsightPage from "./pages/insight";
@@ -12,12 +13,15 @@ import PostPage from "./pages/post";
 import LoginPage from "./pages/login";
 import TagPage from "./pages/tag";
 import SearchPage from "./pages/search";
-import DashboardSlugPage from "./pages/dashboard/slug";
-import DashboardUpdatePage from "./pages/dashboard/update";
+
+const domain = process.env.REACT_APP_DOMAIN_MAIN;
+const imgdomain = process.env.REACT_APP_API_URL;
 
 function App() {
   const location = useLocation();
   const { isLoggedin } = useAuth();
+
+  const isCrawl = typeof window !== "undefined" && window.navigator.userAgent === "IbrahimStudio";
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -27,31 +31,28 @@ function App() {
     // <Fragment>
     //   <WorkerUpdater />
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage imgdomain={imgdomain} />} />
       <Route path="*" element={<ErrorPage />} />
-      <Route path="/informasi/:cslug" element={<CompanyPage />} />
-      <Route path="/berita/insight/:islug" element={<InsightPage />} />
-      <Route path="/berita/kategori/:category" element={<CategoryPage />} />
-      <Route path="/berita/:slug" element={<PostPage />} />
-      <Route path="/berita/tag/:slug" element={<TagPage />} />
-      <Route path="/pencarian/:query" element={<SearchPage />} />
-      <Route path="/login" element={isLoggedin ? <Navigate to="/dashboard" replace /> : <LoginPage />} />
-      <Route path="/dashboard/:scope/:slug" element={isLoggedin ? <DashboardSlugPage /> : <Navigate to="/login" replace />} />
-      <Route path="/dashboard/:uscope/:uslug/update/:params" element={isLoggedin ? <DashboardUpdatePage /> : <Navigate to="/login" replace />} />
+      <Route path="/informasi/:cslug" element={<CompanyPage imgdomain={imgdomain} />} />
+      <Route path="/berita/insight/:islug" element={<InsightPage imgdomain={imgdomain} />} />
+      <Route path="/berita/kategori/:category" element={<CategoryPage imgdomain={imgdomain} />} />
+      <Route path="/berita/:slug" element={<PostPage imgdomain={imgdomain} />} />
+      <Route path="/berita/tag/:slug" element={<TagPage imgdomain={imgdomain} />} />
+      <Route path="/pencarian/:query" element={<SearchPage imgdomain={imgdomain} />} />
+      <Route path="/login" element={isLoggedin ? <RePage to={`${domain}/dashboard`} external /> : <LoginPage />} />
       {/* no-index redirect */}
-      <Route path="/informasi" element={<FallbackPage from="/informasi" to="/informasi/pedoman-media-siber" />} />
-      <Route path="/berita/insight" element={<FallbackPage from="/berita/insight" to="/berita/insight/trending" />} />
-      <Route path="/berita/kategori" element={<FallbackPage from="/berita/kategori" to="/berita/kategori/pifabiz" />} />
-      <Route path="/berita" element={<FallbackPage from="/berita" to="/berita/insight/trending" />} />
-      <Route path="/berita/tag" element={<FallbackPage from="/berita/tag" to="/berita/tag/indonesia" />} />
-      <Route path="/pencarian" element={<FallbackPage from="/pencarian" to="/" />} />
+      <Route path="/informasi" element={isCrawl ? <FallbackPage from="/informasi" /> : <Navigate to="/informasi/pedoman-media-siber" />} />
+      <Route path="/berita/insight" element={isCrawl ? <FallbackPage from="/berita/insight" /> : <Navigate to="/berita/insight/trending" />} />
+      <Route path="/berita/kategori" element={isCrawl ? <FallbackPage from="/berita/kategori" /> : <Navigate to="/berita/kategori/pifabiz" />} />
+      <Route path="/berita" element={isCrawl ? <FallbackPage from="/berita" /> : <Navigate to="/berita/insight/trending" />} />
+      <Route path="/berita/tag" element={isCrawl ? <FallbackPage from="/berita/tag" /> : <Navigate to="/berita/tag/indonesia" />} />
+      <Route path="/pencarian" element={isCrawl ? <FallbackPage from="/pencarian" /> : <Navigate to="/" />} />
       {/* common human assumption redirect */}
       <Route path="/about" element={<Navigate to="/informasi/tentang-pifa" replace />} />
       <Route path="/tentang" element={<Navigate to="/informasi/tentang-pifa" replace />} />
       <Route path="/help" element={<Navigate to="/informasi/faq" replace />} />
       <Route path="/bantuan" element={<Navigate to="/informasi/faq" replace />} />
       <Route path="/masuk" element={<Navigate to="/login" replace />} />
-      <Route path="/dashboard" element={isLoggedin ? <Navigate to="/dashboard/berita/isi-berita" replace /> : <Navigate to="/login" replace />} />
     </Routes>
     // </Fragment>
   );
