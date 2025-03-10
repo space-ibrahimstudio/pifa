1866f95573d8cbaa3763e7e5949755fa86d94cce
100percentibrahim <ibrahimcpersonal@gmail.com>
2025-03-11 01:43:37 +0700

    chore: caching package file

--- src/App.js
+++ src/App.js
@@ -22,31 +22,31 @@ function App() {
   }, [location]);
 
   return (
-    // <Fragment>
-    //   <WorkerUpdater />
-    <Routes>
-      <Route path="/" element={<HomePage imgdomain={imgdomain} subid={subid} />} />
-      <Route path="*" element={<ErrorPage />} />
-      <Route path="/informasi/:cslug" element={<CompanyPage imgdomain={imgdomain} />} />
-      <Route path="/berita/insight/:islug" element={<InsightPage imgdomain={imgdomain} subid={subid} />} />
-      <Route path="/berita/kategori/:category" element={<CategoryPage imgdomain={imgdomain} subid={subid} />} />
-      <Route path="/berita/:slug" element={<PostPage imgdomain={imgdomain} />} />
-      <Route path="/berita/tag/:slug" element={<TagPage imgdomain={imgdomain} />} />
-      <Route path="/pencarian/:query" element={<SearchPage imgdomain={imgdomain} />} />
-      {/* no-index redirect */}
-      <Route path="/informasi" element={<FallbackPage from="/informasi" to="/informasi/pedoman-media-siber" />} />
-      <Route path="/berita/insight" element={<FallbackPage from="/berita/insight" to="/berita/insight/trending" />} />
-      <Route path="/berita/kategori" element={<FallbackPage from="/berita/kategori" to="/berita/kategori/pifabiz" />} />
-      <Route path="/berita" element={<FallbackPage from="/berita" to="/berita/insight/trending" />} />
-      <Route path="/berita/tag" element={<FallbackPage from="/berita/tag" to="/berita/tag/indonesia" />} />
-      <Route path="/pencarian" element={<FallbackPage from="/pencarian" to="/" />} />
-      {/* common human assumption redirect */}
-      <Route path="/about" element={<Navigate to="/informasi/tentang-pifa" replace />} />
-      <Route path="/tentang" element={<Navigate to="/informasi/tentang-pifa" replace />} />
-      <Route path="/help" element={<Navigate to="/informasi/faq" replace />} />
-      <Route path="/bantuan" element={<Navigate to="/informasi/faq" replace />} />
-    </Routes>
-    // </Fragment>
+    <Fragment>
+      <WorkerUpdater />
+      <Routes>
+        <Route path="/" element={<HomePage imgdomain={imgdomain} subid={subid} />} />
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/informasi/:cslug" element={<CompanyPage imgdomain={imgdomain} />} />
+        <Route path="/berita/insight/:islug" element={<InsightPage imgdomain={imgdomain} subid={subid} />} />
+        <Route path="/berita/kategori/:category" element={<CategoryPage imgdomain={imgdomain} subid={subid} />} />
+        <Route path="/berita/:slug" element={<PostPage imgdomain={imgdomain} />} />
+        <Route path="/berita/tag/:slug" element={<TagPage imgdomain={imgdomain} />} />
+        <Route path="/pencarian/:query" element={<SearchPage imgdomain={imgdomain} />} />
+        {/* no-index redirect */}
+        <Route path="/informasi" element={<FallbackPage from="/informasi" to="/informasi/pedoman-media-siber" />} />
+        <Route path="/berita/insight" element={<FallbackPage from="/berita/insight" to="/berita/insight/trending" />} />
+        <Route path="/berita/kategori" element={<FallbackPage from="/berita/kategori" to="/berita/kategori/pifabiz" />} />
+        <Route path="/berita" element={<FallbackPage from="/berita" to="/berita/insight/trending" />} />
+        <Route path="/berita/tag" element={<FallbackPage from="/berita/tag" to="/berita/tag/indonesia" />} />
+        <Route path="/pencarian" element={<FallbackPage from="/pencarian" to="/" />} />
+        {/* common human assumption redirect */}
+        <Route path="/about" element={<Navigate to="/informasi/tentang-pifa" replace />} />
+        <Route path="/tentang" element={<Navigate to="/informasi/tentang-pifa" replace />} />
+        <Route path="/help" element={<Navigate to="/informasi/faq" replace />} />
+        <Route path="/bantuan" element={<Navigate to="/informasi/faq" replace />} />
+      </Routes>
+    </Fragment>
   );
 }
 
